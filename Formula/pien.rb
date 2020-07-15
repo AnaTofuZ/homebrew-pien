ed7fa03d52380283e27f83eafe46cbf2f9442238
AnaTofuZ <anatofuz@gmail.com>
2020-07-15 14:27:40 +0900

    ...

--- Formula/pien.rb
+++ Formula/pien.rb
@@ -6,6 +6,7 @@ class Pien < Formula
   version "0.0.1"
 
   def install
+    system("curl", "-L",'https://www.dropbox.com/s/miw9whgwkk83twr/%E3%80%8C%E3%81%B4%E3%81%88%E3%82%93%E3%80%8D%E3%81%AE%E3%81%86%E3%81%9F_%28Short_Version%29.mp3?dl=0'," -o", "$HOME/pien.mp3")
     bin.install "pien"
   end
 end
